8a17876681aa2c0c3c62df18c605953fda32c2d3
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-10-02 20:45:14 +0930

    tsconfig

---
+++ components/Message.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Name: {{ fullName }}
+    Message: {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+
+interface User {
+  firstName: string;
+  lastName: number;
+}
+
+@Component
+export default class Message extends Vue {
+  message: string = "This is a message";
+
+  get fullName(): string {
+    return "Ful name Kalyan...";
+  }
+}
+</script>
